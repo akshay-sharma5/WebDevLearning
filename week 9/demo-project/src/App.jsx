c6b54d4733c8d28ef3f2d524bdb1267461b33b81
Akshay kumar sharma <akshaykrsharma.official@gmail.com>
2026-09-02 13:32:17 +0000

    added demo project

--- week 9/demo-project/src/App.jsx
+++ week 9/demo-project/src/App.jsx
@@ -1,39 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
+//Conditional rendering
+
 function App() {
 
-  return (
-    <div>
-      <Counter>
+  let [counterVisible,setCounterVisible]=useState(true)
+//mounting logic
+  useEffect(()=>{
+    setInterval(()=>{
+      setCounterVisible(c=>!c)
+    },5000)
 
-      </Counter>
+    
+  },[])
 
-    </div>
+
+  return (  
+  <div>
+    hi
+    {counterVisible && <Counter/>}
+    hello
+  </div>
+
   )
 }
 
-function Counter(){
+function Counter() {
+  const [count, setCount] = useState(0)
 
-  const [count,setCount]=useState(0)
+  /*hooking into lifecycle of events of react
 
-  setInterval(()=>{
-    let time=new Date().toLocaleTimeString()
-     setCount(time)
+  mounting,rendering ,unmounting
 
-  },1000)
+  When we use useEffect the function inside useEffect only mount once
+  If we remove it our clock will go crazy
+  gaurds our setInterval from reRender
+  */ 
 
- 
+  //mounting logic
+  useEffect(() => {
+    console.log("mounting")
+    let clock = setInterval(()=>{
+      console.log("from set interval")
+      setCount(count=>count+1)
+    }, 1000);
+
+    //unmounting logic
+    return ()=>{
+      console.log("unmounting")
+      clearInterval(clock)}
+    
+  }, [])//dependency array ,cleanup,fetch inside userEffect
 
   return (
     <div>
-      <div id='c'>{count}</div>
-      
-
+      <h1>{count}</h1>
     </div>
   )
-  
 }
 
 export default App
